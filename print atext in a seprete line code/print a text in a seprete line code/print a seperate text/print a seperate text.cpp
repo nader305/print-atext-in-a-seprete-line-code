01e82b31308be331e432357e27cb91bf3f42e9e0
nader305 <engnaderwahbaa@gmail.com>
2024-07-12 00:26:45 +0300

    Add files via upload

---
+++ print atext in a seprete line code/print a text in a seprete line code/print a seperate text/print a seperate text.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Print a welcome text in a separate line\n";
+	cout << "=================\n";
+	cout << "Welcome\n";
+	cout << "Nader\n";
+	return 0;
+}
